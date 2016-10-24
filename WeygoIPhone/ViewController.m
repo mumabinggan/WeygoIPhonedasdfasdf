0f3214c4f5f8fc886792365534aa62fbefd27216
zhengyuanqian <zhengyuanqian@lifang.com>
2016-10-24 11:22:39 +0800

    remove libraries

--- WeygoIPhone/ViewController.m
+++ WeygoIPhone/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "JHAlert.h"
 
 @interface ViewController ()
 
@@ -17,8 +18,78 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self testRoundButton1];
+    
+    JHLabel *label = [[JHLabel alloc] initWithFrame:CGRectMake(100, 100, 100, 60)];
+    label.text = @"asdfasd";
+    label.backgroundColor = kRedColor;
+    [self.view addSubview:label];
 }
 
+- (void)showload {
+    [[JHAlert sharedAlert] showLoadingViewWithMessage:@"你好" inView:self.view];
+}
+
+- (void)showWarning {
+    [[JHAlert sharedAlert] showWarningMessageInView:self.view message:@"瞄瞄"];
+}
+
+- (void)showAlert {
+    [[JHAlert sharedAlert] showAlertMessage:@"你好" withTitle:@"你好，早上好"];
+}
+
+- (void)testRoundButton1 {
+    JHButton *button = [[JHButton alloc] initWithFrame:CGRectMake(100, 50, 100, 50) difRadius:JHRadiusMake(5, 7, 10, 15) borderWidth:2 borderColor:kRedColor backgroundColor:kGreenColor];
+    [self.view addSubview:button];
+}
+
+- (void)testRoundButton {
+    CGRect frame = CGRectMake(0, 0, 300, 100);
+    JHRadius difRadius1 = JHRadiusMake(30, 40, 20, 50);
+    UIImage *image1 = [UIImage imageWithRect:frame roundedCorner:difRadius1 borderWidth:8 borderColor:[UIColor redColor] backgroundColor:[UIColor greenColor]];
+    JHButton *button = [[JHButton alloc] initWithFrame:CGRectMake(50, 100, frame.size.width, frame.size.height)];
+    [button setBackgroundImage:image1 forState:UIControlStateNormal];
+    [self.view addSubview:button];
+}
+
+- (void)testSetWebImageView {
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 200, 200, 100)];
+    imageView.backgroundColor = kRedColor;
+    [imageView setImageWithURL:[NSURL URLWithString:@"http://img2.a0bi.com/upload/ttq/20161005/1475660358092.jpg"] placeholderImage:nil options:SDWebImageRefreshCached];
+    [self.view addSubview:imageView];
+}
+
+- (void)testRoundView3 {
+    CGRect frame = CGRectMake(0, 0, 300, 200);
+    JHRadius difRadius = JHRadiusMake(30, 40, 20, 50);
+    JHView *view = [[JHView alloc] initWithFrame:frame difRadius:difRadius backgroundColor:kRedColor];
+    [self.view addSubview:view];
+}
+
+- (void)testRoundView2 {
+    CGRect frame = CGRectMake(0, 0, 300, 200);
+    JHRadius difRadius = JHRadiusMake(30, 40, 20, 50);
+    UIView *view = [[UIView alloc] initWithFrame:frame];
+    [view addCorner:difRadius borderWidth:2.0 borderColor:kRedColor backgroundColor:kGreenColor];
+    [self.view addSubview:view];
+}
+
+- (void)testRoundView {
+    CGRect frame = CGRectMake(0, 0, 300, 200);
+    JHRadius difRadius1 = JHRadiusMake(30, 40, 20, 50);
+    UIImage *image1 = [UIImage imageWithRect:frame roundedCorner:difRadius1 borderWidth:8 borderColor:[UIColor redColor] backgroundColor:[UIColor greenColor]];
+    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:image1];
+    imageView1.frame = CGRectMake(50, 100, frame.size.width, frame.size.height);
+    [self.view addSubview:imageView1];
+}
+
+- (void)testPerformSelector {
+    [self performSelector:@selector(setHHH:ss:) withObjects:@"sd", [NSNull null], nil];
+}
+
+- (void)setHHH:(NSString *)string ss:(NSString *)s {
+    NSLog(@"haha%@%@", string, s);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
